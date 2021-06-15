e8f74613a8e5c7c8268ec81ff046b145a850dc3b
Ayush Purohit <58776569+Ayush-mega-coder@users.noreply.github.com>
2021-06-15 21:58:30 +0530

    Add files via upload

---
+++ Accordian/index.js
@@ -0,0 +1,39 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // // import './index.css';
+// import App from './App';
+// // // import Inc_Dec from './Inc_Dec'
+// // // import Dig from './dig'
+// // import Accordian from './component/accordian/Accordian';
+
+// ReactDOM.render(
+  
+ 
+// {/*<Inc_Dec />
+//     <Dig/>*/},
+//     <App/>,
+//   document.getElementById('root')
+// );
+// // const App = () => {
+// //     return (
+// //         <>
+// //         <Accordian/>
+// //         </>
+// //     )
+// // }
+// // export default App;
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+import './component/accordian/accordian.css'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
